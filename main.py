aaa082acd7468d9e762cf4cb88c6f307558581c9
Lorex <hello@lorex.tw>
2022-11-21 21:07:12 +0800

    Add response handler and random reply.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 #                       #
 #########################
 # LINE Access Token and Secret
-line_access_token = ''
+line_access_token = 'oclPJrW8n+hqEQt5Jc9W+XOI3IZY/isIeVEAwWnhgNDvMRgkfuvRs1OLtdk9qA8GT6JAFMnV15R/rTskSqz+OzLbti6U3MqJif8zTnI6qi50bBrRnhkpFcS5O2bCpGzBA66/fBrG0dwCHuwM5DC/uQdB04t89/1O/w1cDnyilFU='
 
-line_secret = ''
+line_secret = '2f8e7100759b59a7fcfb96b2cd09afd1'
 
 # Turn this off if you don't want to hot reload the pattern file
 hotReloadPattern = True
@@ -17,8 +17,8 @@ hotReloadPattern = True
 #                                  #
 ####################################
 # Import Dependencies
-import json, re
-from flask import Flask
+import json, re, random
+from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
@@ -66,15 +66,26 @@ def messageHandler(message):
 
   # Iterate through matchRegexList and matchStringList
   for item in matchList:
+    # Check if response is a string or a array
     if item['mode'] == 'regex':
       if re.search(item['pattern'], message):
-        return item['response']
+        return responseHandler(item['response'])
 
     if item['mode'] == 'string':
       if item['pattern'] in message:
-        return item['response']
+        return responseHandler(item['response'])
 
-  return 'I don\'t understand what you are saying.'
+  return responseHandler('I don\'t understand what you are saying.')
+
+
+def responseHandler(response):
+  # Check if response is a string or a array
+    if isinstance(response, str):
+      # Response a string message
+      return response
+    else:
+      # Response a random string message from array
+      return response[random.randint(0, len(response) - 1)]
 
 if __name__ == '__main__':
     app.run()
